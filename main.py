5f2815ea6afd3e832ab820d8488e8abb8c88d8ce
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2023-08-27 09:19:12 -0300

    feat: Utilizando metodos de string

--- main.py
+++ main.py
@@ -3,12 +3,35 @@ url = "bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
 
 print(url)
 
+# Find returns character index
+question_mark_index = url.find('?')
+
 # Index from slice are exclusive.
-domain_with_resource = url[0:19]
+domain_with_resource = url[:question_mark_index]
 print(domain_with_resource)
 
-parameters = url[20:36]
+parameters = url[(question_mark_index+1):]
 print(parameters)
 
 # String in Python is Immutable
 print(url)
+
+# Get the parameter value
+value_to_be_found = "moedaDestino"
+# value_to_be_found = "moedaOrigem"
+# value_to_be_found = "quantidade"
+
+# Returns index from initial position from text
+value_to_be_found_index = url.find(value_to_be_found)
+
+# Find method always returns index value for first occurrence.
+# We use find with a second parameter to indicate the initial position it should be start
+concatenation_parameter_index = url.find('&', value_to_be_found_index)
+
+if concatenation_parameter_index == -1:
+    value_found = url[value_to_be_found_index + len(value_to_be_found) + 1:]
+else:
+    value_found = url[value_to_be_found_index + len(value_to_be_found) + 1:concatenation_parameter_index]
+
+print(f"Value found '{value_found}'")
+
